2fb9e30e179ecc81911e86c7ff0c5afe2112d0e1
robertegelund <robert.egelund@outlook.com>
2023-11-09 14:27:32 +0100

    Change the Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app"
+import firebase from "../login.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXv5ksRdJLOS3V8o6JoSYJkYyL5OhX_xY",
+  apiKey: firebase["API_KEY"],
   authDomain: "foreign-aid-kit.firebaseapp.com",
   databaseURL: "https://foreign-aid-kit.firebaseio.com",
   projectId: "foreign-aid-kit",
   storageBucket: "foreign-aid-kit.appspot.com",
-  messagingSenderId: "680222805848",
-  appId: "1:680222805848:web:f373f9fe92c0f979de1f2d"
+  messagingSenderId: firebase["MESSAGING_SENDER_ID"],
+  appId: firebase["APP_ID"]
 };
 
 // Initialize Firebase
